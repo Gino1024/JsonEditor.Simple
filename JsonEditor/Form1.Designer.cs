f730de021721431b844b0946ee6bb3b73cd0fa94
gino <>
2022-09-20 18:22:13 +0800

    init

---
+++ JsonEditor/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace JsonEditor
+{
+    partial class JsonEditor
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.JsonTxt = new System.Windows.Forms.TextBox();
+            this.ConvertToTreeBtn = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // JsonTxt
+            // 
+            this.JsonTxt.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.JsonTxt.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.JsonTxt.Location = new System.Drawing.Point(12, 50);
+            this.JsonTxt.Multiline = true;
+            this.JsonTxt.Name = "JsonTxt";
+            this.JsonTxt.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.JsonTxt.Size = new System.Drawing.Size(355, 614);
+            this.JsonTxt.TabIndex = 0;
+            // 
+            // ConvertToTreeBtn
+            // 
+            this.ConvertToTreeBtn.BackColor = System.Drawing.Color.DarkGray;
+            this.ConvertToTreeBtn.Font = new System.Drawing.Font("新細明體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.ConvertToTreeBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ConvertToTreeBtn.Location = new System.Drawing.Point(406, 285);
+            this.ConvertToTreeBtn.Name = "ConvertToTreeBtn";
+            this.ConvertToTreeBtn.Size = new System.Drawing.Size(52, 52);
+            this.ConvertToTreeBtn.TabIndex = 2;
+            this.ConvertToTreeBtn.Text = ">";
+            this.ConvertToTreeBtn.UseVisualStyleBackColor = false;
+            this.ConvertToTreeBtn.Click += new System.EventHandler(this.ConvertToTreeBtn_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.treeView1.Location = new System.Drawing.Point(497, 50);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(355, 614);
+            this.treeView1.TabIndex = 3;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 35);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Document";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(491, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 35);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Tree";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(774, 680);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Author Gino";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // JsonEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(864, 707);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.ConvertToTreeBtn);
+            this.Controls.Add(this.JsonTxt);
+            this.MaximizeBox = false;
+            this.Name = "JsonEditor";
+            this.Text = "JsonEditor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox JsonTxt;
+        private System.Windows.Forms.Button ConvertToTreeBtn;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ JsonEditor/Form1.cs
@@ -0,0 +1,122 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JsonEditor
+{
+    public partial class JsonEditor : Form
+    {
+        public JsonEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void JsonEditor_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void ConvertToTreeBtn_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Clear();
+
+            string jsonTxt = JsonTxt.Text;
+
+            if (string.IsNullOrEmpty(jsonTxt))
+            {
+                MessageBox.Show("不得為空","err");
+                return;
+            }
+
+            if (!canJsonParse(jsonTxt))
+            {
+                MessageBox.Show("格式有誤","err");
+                return;
+            }
+            var root = Newtonsoft.Json.JsonConvert.DeserializeObject<JObject>(jsonTxt);
+            RenderTreeView(treeView1.Nodes, root);
+
+            JObject json = JObject.Parse(jsonTxt);
+            JsonTxt.Text = json.ToString();
+        }
+
+        private bool canJsonParse(string JsonTxt)
+        {
+            bool result = false;
+            try
+            {
+                var root = JObject.Parse(JsonTxt);
+                result = (root != null) ? true : false;
+            }
+            catch
+            {
+
+            }
+            return result;
+        }
+
+        private void RenderTreeView(TreeNodeCollection nodes, JObject datas)
+        {
+            try
+            {
+                int layer = 0;
+                foreach (JProperty datasProp in datas.Properties())
+                {
+                    bool isDeep = canJsonParse(datasProp.Value.ToString());
+
+                    if (isDeep)
+                    {
+                        nodes.Add($"{datasProp.Name}");
+                        JObject subDatas = Newtonsoft.Json.JsonConvert.DeserializeObject<JObject>(datasProp.Value.ToString());
+                        RenderTreeView(nodes[layer].Nodes,subDatas);
+                    }
+                    else
+                    {
+                        nodes.Add(string.Format("{0} : {1}", datasProp.Name, RenderPropValue(datasProp.Value)));
+                    }
+                    layer++;
+                }
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        private string RenderPropValue(JToken prop)
+        {
+            string result = string.Empty;
+            switch (prop.Type.ToString())
+            {
+                case "String":
+                    result = $"\"{prop}\"";
+                    break;
+                default:
+                    result = string.IsNullOrEmpty(prop.ToString()) ? "null" : prop.ToString();
+                    break;
+            }
+            return result;
+        }
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
